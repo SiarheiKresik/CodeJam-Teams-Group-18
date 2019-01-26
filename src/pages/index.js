13f73952970e3b2e5d92b7e455aebdf8fa17fccf
zzVIPz <viperexe@mail.ru>
2019-01-27 01:12:12 +0300

    feat: add portal and about us modules

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import Image from '../components/image';
+import Portal from '../components/portal';
+import About from '../components/about-us';
 
 const IndexPage = () => (
   <Layout>
     <h1>MAIN PAGE</h1>
-    <p>TODO: about portal component</p>
+    <Portal headerText="123" /> 
     <p>TODO: the person of the day component</p>
-    <p>TODO: about developers component</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
+    <About />
+      <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
   </Layout>
-);
+  );
 
 export default IndexPage;
