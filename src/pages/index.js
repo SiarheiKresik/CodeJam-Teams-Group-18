a42ca1c4c42dbf22864e9562cae7c2ab9d651b81
SiarhiejKresik <siarhei.kresik@gmail.com>
2019-01-26 15:31:41 +0300

    feat: init project via gatsby-cli

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>MAIN PAGE</h1>
+    <p>TODO: about portal component</p>
+    <p>TODO: the person of the day component</p>
+    <p>TODO: about developers component</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+  </Layout>
+)
+
+export default IndexPage
---
+++ src/pages/persons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Persons = () => (
+  <Layout>
+    <h1>PERSONS</h1>
+    <p>TODO: list of persons and the search input</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Persons
