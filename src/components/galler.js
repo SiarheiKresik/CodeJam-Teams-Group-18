684d1d8ebfc858f285a02af6486da816a33635b3
SiarhiejKresik <siarhei.kresik@gmail.com>
2019-01-28 14:30:32 +0300

    refactor: rename the photo gallery component

--- src/components/galler.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Galler = ({ data }) => (
-    <h1>no</h1>
-);
-
-export default Galler;
---
+++ src/components/photo-gallery.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Galler = ({ data }) => <h1>no</h1>;
+
+export default Galler;
