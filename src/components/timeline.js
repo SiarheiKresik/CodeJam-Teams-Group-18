89a26f80015ff79c18f524713b94d1cd2a245f7b
SiarhiejKresik <siarhei.kresik@gmail.com>
2019-01-28 14:27:59 +0300

    fix: repair the timeline component

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+import { Timeline, TimelineItem } from 'vertical-timeline-component-for-react';
 
-const TimelineMy = ({ data }) => (
-    <Timeline lineColor={'#ddd'}>
-    for (key in data){
-        <TimelineItem
-            key={data[key]}
-            dateText={data[dateText]}
-            style={{ color: '#e86971' }}
-        >
-            <p>{data[content]}</p>
-        </TimelineItem>
-        }
-    </Timeline>
+const TimelineWrapper = ({ data }) => (
+  <Timeline>
+    {data.map(obj => (
+      <TimelineItem
+        key={obj.key}
+        dateText={obj.dateText}
+        style={{ color: '#e86971' }}
+      >
+        <p>{obj.content}</p>
+      </TimelineItem>
+    ))}
+  </Timeline>
 );
 
-export default TimelineMy;
+export default TimelineWrapper;
