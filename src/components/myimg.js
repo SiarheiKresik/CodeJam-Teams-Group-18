55d23de7fed55a64562fedfbed291c76b749c020
SiarhiejKresik <siarhei.kresik@gmail.com>
2019-01-28 14:29:53 +0300

    fix: string interpolation in the photo component

--- src/components/myimg.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import './layout.css';
-
-const Myimg = ({ data }) => <img src="../data/pict/"{data}"/1.jpg" />;
-
-export default Myimg;
---
+++ src/components/photo.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './layout.css';
+
+const Photo = ({ data }) => <img src={`../data/pict/${data}/1.jpg`} alt="" />;
+
+export default Photo;
